ce503c1de610219e8bf325b834c62c016c31a4da
DanOnTheMoon21 <45136246+DanOnTheMoon21@users.noreply.github.com>
2019-05-25 01:05:36 -0400

    missed index

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+process.on('unhandledRejection', err => {
+  console.error(err);
+  process.exit(1);
+});
+
+const Manager = require('./src/manager');
+const manager = new Manager();
