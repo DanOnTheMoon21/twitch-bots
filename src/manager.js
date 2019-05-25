5c92a351696efaaca189c761c248e0e30acd7bd7
DanOnTheMoon21 <45136246+DanOnTheMoon21@users.noreply.github.com>
2019-05-25 02:10:02 -0400

    bot status

--- src/manager.js
+++ src/manager.js
@@ -9,7 +9,7 @@ const error = require('./util/error');
 const onMessages = require('./onMessage');
 
 class Manager {
-  constructor () {
+  constructor ({ monitor = true, monitorInterval = 5000 } = {}) {
     this.config = config();
 
     this._log = log.bind(this);
@@ -20,9 +20,19 @@ class Manager {
 
     this.bots = {};
     Object.keys(this.config.bots).forEach(this.create.bind(this));
+
+    if (monitor) {
+      this.monitor = setInterval(() => {
+        this.log('monitor', 'status', this.status());
+      }, monitorInterval);
+    }
   }
 
   create (botName) {
+    if (this.has[botName]) {
+      this.error('create', 'cannot create bot: already exists', new Error('bot already exists'), { botName });
+    }
+
     const botConfig = this.config.bots[botName];
 
     if (botConfig.create) {
@@ -41,6 +51,42 @@ class Manager {
     } else {
       this.log('create', 'skipping bot creation', { botName });
     }
+  }
+
+  has (botName) {
+    return !!this.bots[botName];
+  }
+
+  async exec (botName, botAction, args) {
+    if (!this.has(botName)) {
+      this.log('exec', 'cannot exec: bot does not exist', { botName, botAction, args });
+      return;
+    }
+
+    this.log('exec', 'executing action for bot', { botName, botAction, args });
+    return await this.bots[botName][botAction](...args);
+  }
+
+  statusForBot (botName) {
+    if (!this.has(botName)) {
+      this.log('status', 'cannot get status: bot does not exist', { botName });
+      return;
+    }
+
+    return this.bots[botName].state;
+  }
+
+  status (botName) {
+    if (botName) {
+      return this.statusForBot(botName);
+    }
+
+    const status = {};
+    Object.keys(this.bots).forEach(botName => {
+      status[botName] = this.statusForBot.bind(this)(botName);
+    });
+
+    return status;
   }
 
   log (action, msg, other) {
